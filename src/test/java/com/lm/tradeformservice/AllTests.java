d9a1028f614dcf5b08030f9b334d695d2fa5a4bc
laurentmor <laurent.morissette@gmail.com>
2026-09-06 12:11:44 -0400

    test: replace explicit suite with package-wide discovery
    Add the JUnit platform suite engine and replace the class-by-class FullTestSuite with AllTests, which discovers all tests under com.lm.tradeformservice.

---
+++ src/test/java/com/lm/tradeformservice/AllTests.java
@@ -0,0 +1,13 @@
+package com.lm.tradeformservice;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages("com.lm.tradeformservice")
+@SuiteDisplayName("Full Test Suite")
+public class AllTests {
+
+}
