b9e031c057b7d907e8d664b222caff17b94732da
laurentmor <laurent.morissette@gmail.com>
2026-09-06 11:32:50 -0400

    test(suite): make FullTestSuite explicit
    - Use @SelectClasses to list test classes so the Test Explorer expands the suite
    - No behavior changes; verification: `./mvnw test` passed locally

--- src/test/java/com/lm/tradeformservice/FullTestSuite.java
+++ src/test/java/com/lm/tradeformservice/FullTestSuite.java
@@ -1,12 +1,17 @@
 package com.lm.tradeformservice;
 
 
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
+import com.lm.tradeformservice.dto.TradeformTest;
+import com.lm.tradeformservice.dto.TradeFormStatusTest;
 
 @Suite
-@SelectPackages("com.lm.tradeformservice.dto")
+@SelectClasses({
+	TradeformTest.class,
+	TradeFormStatusTest.class
+})
 @SuiteDisplayName("Full Test Suite")
 public class FullTestSuite {
 
