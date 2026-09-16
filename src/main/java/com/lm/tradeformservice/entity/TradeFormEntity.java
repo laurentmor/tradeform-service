d77f2e58274a7445f6b7daffdd8dd8132b59679b
laurentmor <laurent.morissette@gmail.com>
2026-09-15 21:07:57 -0400

    feat: add TradeForm JPA entity

---
+++ src/main/java/com/lm/tradeformservice/entity/TradeFormEntity.java
@@ -0,0 +1,36 @@
+package com.lm.tradeformservice.entity;
+
+import com.lm.tradeformservice.dto.TradeFormStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor 
+/**
+ * TradeFormEntity class represents the TradeForm entity in the database.
+ * It contains the ID and status of the trade form.
+ */
+public class TradeFormEntity {
+    /**
+     * The unique identifier for the trade form entity.
+     */
+    @Id  
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter 
+    private int id;
+    /**
+     * The status of the trade form entity, represented by the TradeFormStatus enum.
+     */
+    @Getter 
+    @Setter 
+    private TradeFormStatus status=TradeFormStatus.PENDING;
+
+
+    
+}
